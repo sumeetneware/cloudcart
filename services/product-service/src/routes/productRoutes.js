4aec50c4e71d84c254eb1f2f682819fe9620cd3c
sumeetneware <sumeetneware23@gmail.com>
2026-06-20 14:22:41 +0530

    Add JWT protection to product service

--- services/product-service/src/routes/productRoutes.js
+++ services/product-service/src/routes/productRoutes.js
@@ -7,8 +7,35 @@ const {
   createProduct
 } = require("../controllers/productController");
 
-router.get("/products", getProducts);
+const authMiddleware =
+require("../middlewares/authMiddleware");
 
-router.post("/products", createProduct);
+const roleMiddleware =
+require("../middlewares/roleMiddleware");
+
+/*
+==================================
+Protected Routes
+==================================
+*/
+
+router.get(
+  "/products",
+  authMiddleware,
+  getProducts
+);
+
+/*
+==================================
+Admin Only
+==================================
+*/
+
+router.post(
+  "/products",
+  authMiddleware,
+  roleMiddleware("admin"),
+  createProduct
+);
 
 module.exports = router;
