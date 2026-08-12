2b00ad075373ac5b472c49bf1078f87ce0de3ace
Ubuntu <azureuser@cloudcart-vm.rmkw3t11eo4uxk1n3sys1yrjnb.hx.internal.cloudapp.net>
2026-08-12 06:26:15 +0000

    Migrate MySQL connections to Azure

--- services/product-service/src/config/db.js
+++ services/product-service/src/config/db.js
@@ -5,8 +5,13 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
